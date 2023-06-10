603b6e9f773a3b59be1140ea1be4441a7502c3f0
Дмитрий <mitrofan198522@gmail.com>
2023-06-10 18:48:53 +0300

    Add addition

--- src/App/Main.java
+++ src/App/Main.java
@@ -3,6 +3,13 @@ package App;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Super hay!!");
+        int a = 2;
+        int b = 8;
+        int c = add(a,b);
+        System.out.println(c);
+    }
+    private static int add(int a, int b){
+        return a + b;
     }
 
 }
