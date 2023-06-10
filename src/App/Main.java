d7dc585f6a752909a03f832a0aa19ed25a0ddd28
Дмитрий <mitrofan198522@gmail.com>
2023-06-10 18:44:16 +0300

    Update app info

--- src/App/Main.java
+++ src/App/Main.java
@@ -2,7 +2,7 @@ package App;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println("Super hay!!");
     }
-    
+
 }
